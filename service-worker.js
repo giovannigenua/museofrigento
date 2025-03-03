122f5d8ee50e33830153b5a144fd8cab9d2ddae6
giovannigenua <97618529+giovannigenua@users.noreply.github.com>
2025-03-03 16:34:19 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('my-cache').then((cache) => {
-            return cache.addAll([
-                '/', // La home
-                '/index.html', // La tua pagina principale
-                '/style.css', // Eventuali file CSS
-                '/script.js', // Eventuali file JS
-                '/manifest.json', // Il manifesto
-                '/icons/icon-192x192.png', // Icona della PWA
-                '/icons/icon-512x512.png'  // Icona della PWA
-            ]);
-        })
-    );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+          return cache.addAll([
+              '/', // La home
+              '/index.html', // La tua pagina principale
+              '/style.css', // Eventuali file CSS
+              '/script.js', // Eventuali file JS
+              '/manifest.json', // Il manifesto
+              '/icons/icon-192x192.png', // Icona della PWA
+              '/icons/icon-512x512.png'  // Icona della PWA
+          ]);
+      })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
 });
