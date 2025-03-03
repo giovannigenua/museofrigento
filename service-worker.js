9bd9f898518a3385c0f4a73699d7aa3f0eaef2f7
giovannigenua <97618529+giovannigenua@users.noreply.github.com>
2025-03-03 16:15:29 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,24 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return cache.addAll([
-                '/', // La home
-                '/index.html', // La tua pagina principale
-                '/style.css', // Eventuali file CSS
-                '/script.js', // Eventuali file JS
-                '/manifest.json', // Il manifesto
-                '/icons/icon-192x192.png', // Icona della PWA
-                '/icons/icon-512x512.png'  // Icona della PWA
-            ]);
-        })
+      caches.open('virtual-tour-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/script.js',
+          '/images/logo.png',
+          '/images/icon-192x192.png',
+          '/images/icon-512x512.png',
+        ]);
+      })
     );
-});
-
-self.addEventListener('fetch', function(event) {
+  });
+  
+  self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
     );
-});
+  });
+  
